b52a1615a1528bdaafd6f31ced4e4538838c3613
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-26 11:16:52 -0300

    Types comment

--- aula4/index.js
+++ aula4/index.js
@@ -1,6 +1,13 @@
 // Escrever um comentário
 console.log('olá mundo!'); // Aqui tem outro comentário
 console.log('Linha 1');
+// Comentário
+
+/* 
+um bloco de código
+Linha de comentário.
+*/
+
 console.log('Linha 2');
 console.log('Linha 3');
 console.log('Linha 4');
