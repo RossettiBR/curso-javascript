1470c27e6c45cc9f98f018dbad98ed9274087764
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-26 10:55:57 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Wanderson Rossetti');
+console.log("Wanderson 'Rossetti'");
+console.log(`Wanderson Rossetti`);
+
+console.log(123456, 15.85);
+console.log(12, 45, 37.87, 'Wanderson Rossetti');
