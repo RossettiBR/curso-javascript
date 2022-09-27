42a029d3f4d88c1ff30fa246125bc4bf66b9ffd2
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-27 14:02:32 -0300

    create tree index.js

---
+++ aula6/index2.js
@@ -0,0 +1,5 @@
+let nome; // Declarada a variárivel
+nome = 'João'; // Inicializando a variável
+console.log(nome);
+nome = 'Wander';
+console.log(nome);
---
+++ aula6/index3.js
@@ -0,0 +1,8 @@
+// Não podemos criar variáveis com palavras declaradas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome da variável com número
+// Não podem conter espaços ou traços
+// Urtilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não UTILIZE VAR, UTILIZE LET.
