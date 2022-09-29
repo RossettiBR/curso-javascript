47dbc8337b871c1cbaa5ef98d09a8e064b2d70c3
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-29 16:35:04 -0300

    create aula11

---
+++ aula11/index.js
@@ -0,0 +1,32 @@
+/**
+ * Aritméticos 
+ * + Adição / Concatenação
+ * - / * 
+ * ** = potenciação
+ * % Resto da divisão
+ * Incremento = ++
+ * Decremento = --
+ * NaN - Not a number
+ */ 
+
+const num1 = 10;
+const num2 = 3;
+console.log(num1 % num2);
+
+let contador = 1;
+++contador;
+++contador;
+++contador;
+++contador;
+console.log(contador);
+
+const passo = 2;
+let contaPasso = 0;
+
+contaPasso = contaPasso + passo;
+contaPasso += 2; // contaPasso = contaPasso + passo
+contaPasso += 2;
+contaPasso += 2;
+contaPasso += 2;
+console.log(contaPasso)
+
