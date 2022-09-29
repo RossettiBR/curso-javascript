9de259d4e9075751a092abe40e3d77e7af326d92
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-29 10:00:56 -0300

    create aula7

---
+++ aula7/index.js
@@ -0,0 +1,11 @@
+// Não podemos criar constantes com palavras declaradas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome da constante com número
+// Não podem conter espaços ou traços
+// Urtilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// Não UTILIZE VAR, UTILIZE CONST.
+
+const nome = 'João';
+console.log(nome);
