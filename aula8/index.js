08d197ce30f9c3926e492563e496be163f6b9fbf
Wanderson Rossetti <pastarossetti@gmail.com>
2022-09-29 14:30:36 -0300

    create aula8

---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Wanderson';
+const sobrenome = 'Rossetti';
+const idade = 32;
+const peso = 92;
+const alturaEmM = 1.72;
+let imc = peso / (alturaEmM * alturaEmM);
+let anoNascimento = 2022 - idade
+console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg');
+console.log('tem', alturaEmM, 'm de altura e seu IMC é de', imc);
+console.log(nome, 'nasceu em', anoNascimento);
