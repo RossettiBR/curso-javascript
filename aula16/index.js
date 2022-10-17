e6b961f6f4c1672fa90a2e18b1a6a5981a4da7bc
Wanderson Rossetti <pastarossetti@gmail.com>
2022-10-17 15:04:44 -0300

    create aula16

---
+++ aula16/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+let num2 = Math.round(num1);
+console.log(num2);
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
+
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
