f7a352dd90db1c334cd1667bd37fd857dce0f804
Wanderson Rossetti <pastarossetti@gmail.com>
2022-10-31 15:22:42 -0300

    create aula19

---
+++ aula19/index.js
@@ -0,0 +1,23 @@
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`;
+// }
+
+// const variavel = saudacao('Wander');
+// console.log(variavel);
+
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// console.log(soma(1, 4));
+// console.log(soma(6, 9));
+// console.log(soma(9, 10));
+
+const raiz = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(25));
+console.log(raiz(36));
