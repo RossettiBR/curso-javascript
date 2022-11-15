3fbd59f4482e724dc67498a699d2516b5bb14eae
Wanderson Rossetti <pastarossetti@gmail.com>
2022-11-15 20:49:03 -0300

    create logica

---
+++ aula23 - logica/index.js
@@ -0,0 +1,12 @@
+// function falaOi () {
+//     return 'Oi';
+// }
+
+// const vaiExecutar = 'Joãozinho';
+
+// console.log(vaiExecutar && falaOi());
+
+const corUsuario = 'vermelho';
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
