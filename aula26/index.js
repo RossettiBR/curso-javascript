3e550cd57b3d2fbd003611d131070abb709a5387
Wanderson Rossetti <pastarossetti@gmail.com>
2022-12-20 08:41:16 -0300

    create aula26

---
+++ aula26/index.js
@@ -0,0 +1,8 @@
+// ? :
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário vip' : 'Usuário Normal';
+
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+console.log(nivelUsuario , corPadrao);
