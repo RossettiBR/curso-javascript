fcabfd72867281cbbb6b6393c72892bfc1d11351
Wanderson Rossetti <pastarossetti@gmail.com>
2022-12-20 09:25:58 -0300

    create aula27

---
+++ aula27/index.js
@@ -0,0 +1,33 @@
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(1671538356107);
+// console.log(data.toString());
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth());
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('MS', data.getMilliseconds());
+// console.log('Dia semana', data.getDay());
+// console.log(Date.now());
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth());
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+    const ms = zeroAEsquerda(data.getMilliseconds());
+    const diaSemana = data.getDay();
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil)
